a53ac8cbe010c09b6ba166dc7fcd62c65def1dd0
bread69420 <99593800+bread69420@users.noreply.github.com>
2022-04-10 18:24:27 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,5 +7,14 @@ using namespace std;
 
 
 int main(){
-  
+  string equation = "3*2=6";
+  string guess;
+  bool Guess_correct;
+  while (!Guess_correct){
+    cin >> guess;
+    if (guess == equation){
+      Guess_correct = true;
+    }
+  }
+  cout << "You win!" << endl;
 }
