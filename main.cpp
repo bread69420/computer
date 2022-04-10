c249a7f9cf04e421095af91fc2e03e8433b11fa9
bread69420 <99593800+bread69420@users.noreply.github.com>
2022-04-10 18:55:01 +0800

    Update main.cpp
    demo equation: 3*2=6
    features: take count, reject incorrect length input

--- main.cpp
+++ main.cpp
@@ -2,19 +2,34 @@
 #include <cmath>
 #include <iomanip>
 #include <cstring>
+#include <string>
 using namespace std;
 
+const int MAX = 20;
 
+void accept_guess(string &s, int len){
+  cin >> s;
+  while (s.length() != len){
+    cout << "Invalid guess." << endl;
+    cin >> s;
+  }
+}
 
 int main(){
   string equation = "3*2=6";
   string guess;
+  string attempt_check;
   bool Guess_correct;
+  int equation_len, count, eq_len;
+  eq_len = equation.length();
+  count = 0;
   while (!Guess_correct){
-    cin >> guess;
+  accept_guess(guess, eq_len);
     if (guess == equation){
       Guess_correct = true;
     }
+    count++;
+
   }
-  cout << "You win!" << endl;
+  cout << "You win!\nYou toke " << count << " times to get the answer." << endl;
 }
